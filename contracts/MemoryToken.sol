9bdde5a845d10f5a367904fdcdecaa2febdbec8e
Bruno Figueiredo Nogueira <bruno@brunon.com.br>
2022-02-03 22:19:13 -0300

    Inclui referência ao OpenZeppelin e faz o contrato herdar o padrão ERC721, implementando função mint

--- contracts/MemoryToken.sol
+++ contracts/MemoryToken.sol
@@ -1,6 +1,25 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.4.22 <0.9.0;
+pragma solidity ^0.8.0;
 
-contract MemoryToken {
-    
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract MemoryToken is ERC721URIStorage {
+
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIdCounter;
+
+    constructor() ERC721("Memory Token", "MTT") {}
+
+    function mint(address wallet, string memory _tokenURI) public returns(uint256) {
+        uint256 tokenId = _tokenIdCounter.current();
+
+        _safeMint(wallet, tokenId);
+        _setTokenURI(tokenId, _tokenURI);
+
+        _tokenIdCounter.increment();
+
+        return tokenId;
+    }
 }
