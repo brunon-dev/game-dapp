b75891de1b361262d84f707e59760d32ffaa6d5b
Bruno Figueiredo Nogueira <bruno@brunon.com.br>
2022-02-05 23:47:32 -0300

    Cria os testes relacionados com o mint e adiciona a herança de ERC721Enumerable no nosso contrato, implementando o override das funções '_beforeTokenTransfer', '_burn', 'tokenURI' e 'supportsInterface'

--- test/MemoryTokenTest.js
+++ test/MemoryTokenTest.js
@@ -1,10 +1,12 @@
+const { assert } = require('chai');
+
 const MemoryToken = artifacts.require('./contracts/MemoryToken.sol');
 
 require('chai')
     .use(require('chai-as-promised'))
     .should()
 
-contract('Memory Token', (acounts) =>{
+contract('Memory Token', (accounts) =>{
 
     let token
 
@@ -28,6 +30,41 @@ contract('Memory Token', (acounts) =>{
         it('has a symbol', async () => {
             const symbol = await token.symbol()
             assert.equal(symbol, 'MTT')
+        })
+    })
+
+    describe('token distribution', async () => {
+        let result
+
+        it('mints token', async () => {
+            await token.mint(accounts[0], 'https://www.token-uri.com/nft')
+
+            // It shoul increase the total supply
+            result = await token.totalSupply()
+            assert.equal(result.toString(), '1', 'total supply is correct')
+
+            // It increments owner balance
+            result = await token.balanceOf(accounts[0])
+            assert.equal(result.toString(), '1', 'balanceOf is correct')
+
+            // Token should belong to owner
+            result = await token.ownerOf('0')
+            assert.equal(result.toString(), accounts[0].toString(), 'ownerOf is correct')
+            result = await token.tokenOfOwnerByIndex(accounts[0], 0)
+
+            // Owner can see all tokens
+            let balanceOf = await token.balanceOf(accounts[0])
+            let tokenIds = []
+            for(let i = 0; i < balanceOf; i++) {
+                let id = await token.tokenOfOwnerByIndex(accounts[0], i)
+                tokenIds.push(id.toString())
+            }
+            let expected = ['0']
+            assert.equal(tokenIds.toString(), expected.toString(), 'tokenIds are correct')
+
+            // Token URI Correct
+            let tokenURI = await token.tokenURI('0')
+            assert.equal(tokenURI, 'https://www.token-uri.com/nft')
         })
     })
 })
